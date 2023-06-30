c69abdc8fc7a0abdf20d2cbdadabbb9eb63ed4ad
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-06-30 19:23:54 +0200

    added index.php file to project and added form

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <form action="./server.php" method="GET">
+            <input type="text" name="paragraph">
+            <input type="text" name="badword">
+            <button type="submit">
+                Invia il form
+            </button>
+</body>
+</html>
